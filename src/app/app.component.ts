f758ff5ef59fdb19a1527630661761f5f168ea26
simo96-pr@hotmail.it <simo96-pr@hotmail.it>
2019-02-27 10:49:43 +0100

    Adds header component and style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'first-ang';
+
+  componentTitle = "I am component from component.ts";
+
+  clickHandler() {
+    alert("I AM CLICKED");
+  }
+
+
 }
---
+++ src/app/common/header/header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ang-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+})
+
+export class HeaderComponent {
+
+}
